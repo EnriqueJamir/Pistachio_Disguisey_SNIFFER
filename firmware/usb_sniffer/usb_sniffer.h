e880667b2e8ebacc950daa77e219752e9cf1f84b
Enrique Jamir <121876405+EnriqueJamir@users.noreply.github.com>
2025-09-20 14:32:35 -0300

    Atualizar o usb_sniffer.h

--- firmware/usb_sniffer/usb_sniffer.h
+++ firmware/usb_sniffer/usb_sniffer.h
@@ -1,23 +1,9 @@
-// Parte 1 – Thread 1: USB Sniffer Core
-#ifndef USB_SNIFFER_H
-#define USB_SNIFFER_H
+// usb_sniffer.h
+#pragma once
+#include "buffer.h"
+#include "types.h"
 
-#include <stdint.h>
-#include <stdbool.h>
-
-typedef struct {
-    uint8_t *data;
-    uint16_t length;
-    uint64_t timestamp;
-} Packet;
-
-// Inicializa o módulo USB Sniffer
-void initUSBSniffer();
-
-// Captura pacotes USB M3 <-> M4
-Packet captureUSB();
-
-// Watchdog: detecta falhas no USB e reinicia se necessário
-void resetStealthIfError(Packet p);
-
-#endif // USB_SNIFFER_H
+Packet captureUSB(Device src, Device dst);
+bool detectErrorUSB(Packet p);
+void resetStealth();
+unsigned long getCurrentTime();
