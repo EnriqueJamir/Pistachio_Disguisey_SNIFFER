9da578af86ccb3e2a495095cefc2b75e3a271a7b
Enrique Jamir <121876405+EnriqueJamir@users.noreply.github.com>
2025-09-20 14:41:01 -0300

    Atualizar o parser.h

--- firmware/parser_logger/parser.h
+++ firmware/parser_logger/parser.h
@@ -1,22 +1,8 @@
-// Parte 3 – Thread 3: Parser & Logger
-#ifndef PARSER_H
-#define PARSER_H
+// parser.h
+#pragma once
+#include "types.h"
+#include "buffer.h"
 
-#include "../buffer_inteligente/buffer.h"
-#include <string>
-#include <vector>
-
-// Estrutura de log
-struct LogEntry {
-    std::string tipo_transacao;
-    size_t tamanho;
-    std::string conteudo_relevante;
-    uint64_t timestamp;
-};
-
-// Funções principais
-LogEntry parse_pacote(const Packet& pacote);
-void criar_log(const LogEntry& log);
-void gravar_memoria_criptografada(const LogEntry& log);
-
-#endif // PARSER_H
+LogEntry parseTransaction(Packet packet);
+int calculatePriority(Packet packet);
+LogEntry compressLogAdaptive(LogEntry log);
